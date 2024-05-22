bb3d087b34a2a65a8365ec12618ce8856baaff2a
LaurinhaNasa <laurafercordeiro@gmail.com>
2024-05-22 10:13:15 -0300

    Criação dos marcadores na classe Worker.java

--- src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/model/domain/Worker.java
+++ src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/model/domain/Worker.java
@@ -4,12 +4,38 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="workers")
 public class Worker implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(name = "birthDate", nullable = false, columnDefinition = "DATE")
+    @DateTimeFormat(iso = ISO.DATE)
     private LocalDate birthDate;
+
+    @Column(name = "salary", nullable = false)
     private BigDecimal salary;
+
+    @Column(nullable = false)
     private String role;
     
     public Worker(String name, String email, LocalDate birthDate, BigDecimal salary, String role) {
