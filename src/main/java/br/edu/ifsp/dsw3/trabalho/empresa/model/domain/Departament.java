fde196dce907ecb4be6e8f174c5b7ebf7ea1b217
LaurinhaNasa <laurafercordeiro@gmail.com>
2024-05-22 11:27:02 -0300

    Criação dos marcadores na classe Departament

--- src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/model/domain/Departament.java
+++ src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/model/domain/Departament.java
@@ -2,11 +2,34 @@ package br.edu.ifsp.dsw3.trabalho.empresa.model.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Departaments")
 public class Departament implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String address;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String description;
+
+    @OneToMany
+    @JoinColumn(name="departament_id", nullable=false)
     private Worker worker;
     
     public Departament(Long id, String address, String name, String description, Worker worker) {
