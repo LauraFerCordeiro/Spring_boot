f878c84d15f21ec07f24a70a30454353353e4413
LaurinhaNasa <laurafercordeiro@gmail.com>
2024-05-29 09:24:49 -0300

    Listar todos em departament e worker

--- src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/controller/DepartamentController.java
+++ src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/controller/DepartamentController.java
@@ -1,6 +1,9 @@
 package br.edu.ifsp.dsw3.trabalho.empresa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,5 +21,10 @@ public class DepartamentController {
     @PostMapping("/register")
     public Departament cadastrarDepartament(@RequestParam(value = "address") String address, @RequestParam(value = "name") String name, @RequestParam(value = "description") String description){
         return dao.save(new Departament(address, name, description));
+    }
+
+    @GetMapping("/listar")
+    public List<Departament> listar(){
+        return dao.findAll();
     }
 }
--- src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/controller/WorkerController.java
+++ src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/controller/WorkerController.java
@@ -2,8 +2,10 @@ package br.edu.ifsp.dsw3.trabalho.empresa.controller;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,5 +29,10 @@ public class WorkerController {
     public Worker registerWorker(@RequestParam(value = "name") String name, @RequestParam(value = "email") String email, @RequestParam(value = "birthDate") LocalDate birthDate, @RequestParam(value = "salary") BigDecimal salary, @RequestParam(value = "role") String role, @RequestParam(value = "departamentId") Long departamentId){
         Departament d = daoD.findById(departamentId).get();
         return dao.save(new Worker(name, email, birthDate, salary, role, d));
+    }
+
+    @GetMapping("/listar")
+    public List<Worker> listar(){
+        return dao.findAll();
     }
 }
