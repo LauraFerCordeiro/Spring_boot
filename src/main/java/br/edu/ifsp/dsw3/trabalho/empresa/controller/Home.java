3a86c041b55ce9f39c6562e905df635c08906185
MateusRibPereira <mateus14alek@gmail.com>
2024-09-17 02:18:07 -0300

    Criando footer e header fragments

--- src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/controller/Home.java
+++ src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/controller/Home.java
@@ -11,4 +11,39 @@ public class Home {
     public String principal(){
         return "/home";
     }
+
+    @GetMapping("/contato")
+    public String contato() {
+        return "pages/contato";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "pages/login";
+    }
+
+    @GetMapping("/parceiros")
+    public String parceiros() {
+        return "pages/parceiros";
+    }
+
+    @GetMapping("/quem_somos")
+    public String quemSomos() {
+        return "pages/quem_somos";
+    }
+
+    @GetMapping("/registro_empresa")
+    public String registroEmpresa() {
+        return "pages/registro_empresa";
+    }
+
+    @GetMapping("/registro_pessoa")
+    public String registroPessoa() {
+        return "pages/registro_pessoa";
+    }
+
+    @GetMapping("/registro")
+    public String registro() {
+        return "pages/registro";
+    }
 }
