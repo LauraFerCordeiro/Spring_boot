929cf66d45e7bbafae611fb4743669359a41d9a1
MateusRibPereira <mateus14alek@gmail.com>
2024-12-04 05:49:17 -0300

    Adicionando admin no WebSecurity

--- src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/page_controller/WebSecurityConfig.java
+++ src/main/java/br/edu/ifsp/dsw3/trabalho/empresa/page_controller/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig {
             .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/css/*", "/images/*","/","/contato", "/login", "/parceiros", "/quem_somos", "/registro", "/registro_pessoa", "/h2-console/**", "/accounts/person/salvar").permitAll()
                 .requestMatchers("/people/home", "/people/meuscursos", "/people/todoscursos", "/courses/course/*").hasRole("PERSON")
-                .requestMatchers( "/courses/**", "/lessons/**" , "/people/**").hasRole("ADMIN")
+                .requestMatchers( "/courses/**", "/lessons/**" , "/people/**", "/dashboard/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
             
